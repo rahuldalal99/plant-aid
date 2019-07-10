7b65aef3be4b1a67e6d7619430a7f7b1448b52c9
root <tanaykarve@gmail.com>
2019-07-10 13:19:17 +0000

    fixed auto install file

--- model/install.sh
+++ model/install.sh
@@ -1,6 +1,6 @@
 #ubuntu only
 
-apt-get update
+apt-get update -y
 apt-get install python3 python3-pip
 pip3 install -r requirements.txt
-echo "[+] All done, start app.
+echo "[+] All done, start app."
