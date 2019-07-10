f744a78d7372298ab68a2141721be2ddab83d0cd
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2019-07-10 10:56:30 +0530

    from lab

--- PHP/upload.php
+++ PHP/upload.php
@@ -1,14 +1,47 @@
 <?php
 //Working
 //TODO: save file from requests by arbitrary ID, and use that ID below
-$ch = curl_init();
-$cfile = curl_file_create('esca.jpg','image/jpeg','testpic'); //replace esca.jpg with uploaded filename
-$data=array("image"=>$cfile);
+echo "
 
-curl_setopt($ch, CURLOPT_URL, 'http://5e82856a.ngrok.io/predict');//ngrok is a tunnel to my machine
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+<!DOCTYPE html>
+<html>
+<body>
 
-$response = curl_exec($ch);
-print_r($response);
+<form action=\"upload.php\" method=\"post\" enctype=\"multipart/form-data\">
+    Select image to upload:
+    <input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">
+    <input type=\"submit\" value=\"Upload Image\" name=\"submit\">
+</form>
+
+</body>
+</html>
+
+";
+//TODO: implement select list for plant leaf name;
+// [ Grape , Strawberry, Potato, Tomato ]
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+if(isset($target_file)){
+	$ch = curl_init();
+	$cfile = curl_file_create($target_file,'image/jpeg','testpic'); //replace esca.jpg with uploaded filename
+	$data=array("image"=>$cfile);
+	curl_setopt($ch, CURLOPT_URL, 'http://autumn.ai/predict');//ngrok is a tunnel to my machine
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	$response = curl_exec($ch);
+	print("response");
+	echo("<div id =jso type=hidden>$response<div>");
+	echo("<script>	
+	jso=document.getElementById('jso');
+	if(jso!=null){
+		alert(jso.firstChild);
+		jso=JSON.parse(jso.firstChild.textContent);
+		alert(jso);
+	}
+</script>");
+	//REDIS procedure
+}
+
+//'{ "Pepper,_Bell___Bacterial_Spot": 4.872653789789183e-06, "Pepper,_Bell___Healthy": 2.5014212923224477e-08, "Tomato___Early_Blight": 1.5270050823801284e-07, "Tomato___Tomato_Yellow_Leaf_Curl_Virus": 0.999994993209838}'
 ?>
