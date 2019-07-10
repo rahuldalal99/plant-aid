ac972d9288d164d1a2af56cd376f3357c3839616
rahuldalal99 <46849040+rahuldalal99@users.noreply.github.com>
2019-07-10 17:46:28 +0530

    Update user.sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,1 +1,12 @@
+CREATE TABLE User(
+  email_id email primary key, 
+  name varchar(100),
+  passwd varchar(50),
+  pr_image_path varchar(350)
+);
 
+CREATE TABLE User_images(
+  email_id email references User(email_id)on delete cascade,
+  image_path varchar(350),
+  prediction varchar(100)
+  );
