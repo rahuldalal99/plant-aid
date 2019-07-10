5a40d540f23e04c8f452d34d1f54541ec78b8b3c
Tanay Karve <tanaykarve@gmail.com>
2019-07-10 20:29:43 +0530

    Created upload file function

--- PHP/upload.php
+++ PHP/upload.php
@@ -1,6 +1,21 @@
 <?php
+session_start();
 //Working
 //TODO: save file from requests by arbitrary ID, and use that ID below
+function uploadFile($target_dir="uploads",$type){ //type = {'profile','plant'} returns path to uploaded and moved file
+	$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+	$extension=pathinfo($target_file)['extension'];
+	switch($type){
+		case "profile": $fName=md5($_SESSION['email']);		
+				$fName=$fName.".".$extension;
+				break;
+		case "plant":	$fName=md5($_SESSION['email'].date('d-m-Y').date("h-i-s",time().$extension);
+				break;
+		case default: die("[!]Error in type");
+	}
+	rename($fName,$target_dir."/".$fName);
+	return ($target_dir."/".$fName);
+}
 echo "
 
 <!DOCTYPE html>
@@ -19,7 +34,7 @@ echo "
 ";
 //TODO: implement select list for plant leaf name;
 // [ Grape , Strawberry, Potato, Tomato ]
-
+uploadFile('uploads','plant');
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 if(isset($target_file)){
 	$ch = curl_init();
