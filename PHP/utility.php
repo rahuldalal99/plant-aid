8913e4076c31529f2373d70a6747e117d2c8f813
Tanay Karve <tanaykarve@gmail.com>
2019-07-12 14:15:28 +0000

    Update utility.php

--- PHP/utility.php
+++ PHP/utility.php
@@ -1,19 +1,25 @@
 <?php
 session_start();
+$_SESSION['email']='tanay@gmail.com';
 //Working
 //TODO: save file from requests by arbitrary ID, and use that ID below
 function uploadFile($target_dir="uploads",$type){ //type = {'profile','plant'} returns path to uploaded and moved file
-	$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+	$target_file = basename($_FILES["fileToUpload"]["name"]);
 	$extension=pathinfo($target_file)['extension'];
+	$_SESSION['EXT']=$extension;
 	switch($type){
 		case "profile": $fName=md5($_SESSION['email']);		
 				$fName=$fName.".".$extension;
 				break;
-		case "plant":	$fName=md5($_SESSION['email'].date('d-m-Y').date("h-i-s",time().$extension));
+		case "plant":	$dateNow=date('d-m-Y');
+		                $timeNow=date("h-i-s",time());
+		                $fName=md5($_SESSION['email'].$dateNow.$timeNow).'.'.$extension;
 				break;
 		default: die("[!]Error in type");
 	}
-	rename($fName,$target_dir."/".$fName);
+	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_dir."/".$fName);
+	$_SESSION['DATE_NOW']=$dateNow;
+	$_SESSION['TIME_NOW']=$timeNow;
 	return ($target_dir."/".$fName);
 }
 ?>
