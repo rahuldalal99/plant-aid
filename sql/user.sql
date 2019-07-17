e67fa70e7e12b9798d2108b6df63f234d6304dc2
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2019-07-17 10:16:05 +0530

    update user.sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,12 +1,13 @@
-CREATE TABLE User(
-  email_id email primary key, 
+CREATE TABLE Users(
+  email_id varchar(100),
   name varchar(100),
   passwd varchar(50),
   pr_image_path varchar(350)
 );
 
 CREATE TABLE User_images(
-  email_id email references User(email_id)on delete cascade,
+  email_id varchar(100) references Users(email_id)on delete cascade,
   image_path varchar(350),
-  prediction varchar(100)
+  prediction varchar(100),
+  date_upload timestamp
   );
