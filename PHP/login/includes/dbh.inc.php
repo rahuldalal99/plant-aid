e0d4c2eac904e9d85544ac77f689e23289ced297
rahuldalal99 <46849040+rahuldalal99@users.noreply.github.com>
2019-08-14 01:11:05 -0400

    Update dbh.inc.php

--- PHP/login/includes/dbh.inc.php
+++ PHP/login/includes/dbh.inc.php
@@ -4,10 +4,9 @@
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "Users";*/
-$str = "host=localhost dbname=postgres user=postgres password=sas-fc@22";
+$str = "host=localhost dbname=postgres user=postgres password=postgres";
 
 $conn = pg_connect($str);
 if(!$conn){
 	echo "Cannot connect: ". pg_last_error($conn);
 }
-
