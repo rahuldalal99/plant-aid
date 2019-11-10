186a2ea80c6744d000d8bf1ffbafabaf8cf7fcc0
Tanay Karve <tanaykarve@gmail.com>
2019-11-10 06:05:49 +0530

    Update application.py

--- Flask/application.py
+++ Flask/application.py
@@ -1,11 +1,18 @@
+#!/usr/bin/python
 from flask import Flask,render_template, request, make_response, jsonify,session,redirect,url_for,g
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import default_exceptions, HTTPException
 import os
+from preventor import *
+from API import *
 from pymongo import MongoClient
 import requests
-import json
-
+kn = pickle.load(open("pfiles/KNN.pkl","rb"))
+svm = pickle.load(open("pfiles/SVM.pkl","rb"))
+dt = pickle.load(open("pfiles/DT.pkl","rb"))
+rf = pickle.load(open("pfiles/RF.pkl","rb"))
+lr = pickle.load(open("pfiles/LR.pkl","rb"))
+gnb = pickle.load(open("pfiles/GNB.pkl","rb"))
 
 mongo = MongoClient()
 db = mongo.medivine
@@ -14,14 +21,124 @@ users=db.users
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24) #secret key for sessions
-SAVEDIR="/root/plant-aid/Flask/static/"
+SAVEDIR="/root/plant-aid/Flask/uploads/"
 
-app.config['UPLOADED_FILES_DEST'] = '/c/Users/Rahul/Downloads/flask_app/uploads'
+#app.config['UPLOADED_FILES_DEST'] = '/c/Users/Rahul/Downloads/flask_app/uploads'
+
+
+# -*- coding: utf-8 -*-
+
+app = Flask(__name__)
+
+
+@app.route('/cure/<plant>/<disease>',methods = ["POST","GET"])
+def cure(plant, disease):
+    print(plant)
+    data = json.load(open("cure.json", "r", encoding="utf-8"))
+    return render_template("cure.html",plant=plant, disease=disease,data = data)
+
+@app.route('/sup')
+def sup():
+    return render_template("supportform.html")
+
+@app.route('/support',methods=["POST"])
+def insert():
+    try:
+        if 1:#request.method == "POST":
+            info = request.form
+            rec = {}
+            #u_id = info['user_id']
+            #info.pop('user_id',None)
+            #del info['user_id']
+            print("************")
+            #info = json.dumps(info)
+            print(request.form)
+            rec['user_id'] = info['user_id']
+            rec['disease_title'] = info['disease_title']
+            rec['causes']=info['causes']
+            rec['cure']=info['cure']
+            rec['text']=info['text']
+            db.support.insert_one(rec)
+            print(rec)
+            return render_template("index.html")
+
+    except Exception as e:
+        print(e)
+        return render_template("index.html")
+
+
+
+@app.route('/prev/')
+def prev():
+    url = 'https://geo.p.rapidapi.com/'
+
+    headers = {'x-rapidapi-host': 'geo.p.rapidapi.com',
+               'x-rapidapi-key': 'e169a4fe04msh4afb825ab06053fp1dc5ddjsna23987d08d7a'}
+
+    response = requests.request('GET', url, headers=headers)
+    J = json.loads(response.text)
+    lon = J['longitude']
+    lat = J['latitude']
+    (t, h) = getWeather(lon=lon, lat=lat)
+    preds = []
+    freq = []
+    preds.append(kn.predict([[t, h]]).tolist())
+    preds.append(svm.predict([[t, h]]).tolist())
+    preds.append(dt.predict([[t, h]]).tolist())
+    preds.append(rf.predict([[t, h]]).tolist())
+    preds.append(lr.predict([[t, h]]).tolist())
+    preds.append(gnb.predict([[t, h]]).tolist())
+    for j in range(0, 3):
+        counter = 0
+        num = preds[0]
+
+      # finding most freq
+
+        for i in preds:
+            curr_frequency = preds.count(i)
+            if curr_frequency > counter:
+
+              # runner_up = num
+
+                counter = curr_frequency
+                num = i
+
+        freq.append(num)
+
+      # removing most frequent from list
+
+        for i in preds:
+            if i == num:
+                preds.remove(i)
+
+        if len(preds) == 0:
+            if j == 1:
+                freq[1] = freq[0]
+                freq[2] = freq[0]
+                break
+            if j == 2:
+                freq[2] = freq[0]
+                break
+
+    freq = Remove(freq)
+    freq1 = json.dumps({'prediction': freq})
+    print ('***********freq 1 : ', freq1)
+    return freq1
+
+
+    # return render_template("ind.html",jf =freq1)
+
+def Remove(duplicate):
+    final_list = []
+    for num in duplicate:
+        if num not in final_list:
+            final_list.append(num)
+    return final_list
+
 
 @app.route("/")
 def index():
     return  render_template("index.html")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -81,12 +198,10 @@ def upload():
         #print("File uploaded")
         #print(file)
         #file.save(os.path.join(app.config['UPLOADED_FILES_DEST'], file.filename))
-        path="IMG"+str(len(os.listdir(SAVEDIR))+1)+".jpg"
+        path="IMG"+str(len(os.listdir(SAVEDIR)))+".jpg"
         print(path)
         pathIMG=os.path.join(SAVEDIR, path)
         file.save(pathIMG)
-        print(pathIMG)
-        print("PATH^^")
         #path=urlencode(path)
         #print("http://medivine.me:5000?img_path="+path)
         #file.save(pathIMG)
@@ -108,46 +223,11 @@ def upload():
         print(plant)
         print(disease)
         #res = make_response(jsonify({"message": "File uploaded"}), 200)
-        res = make_response(jsonify({"message": "File uploaded","path":path,"plant":plant,"disease":disease}), 200)
+        res = make_response(jsonify({"message": "File uploaded","plant":plant,"disease":disease}), 200)
+        
         return res
 
-    return render_template("upload1.html")
-
-@app.route('/cure/<plant>/<disease>',methods = ["POST","GET"])
-def cure(plant, disease):
-    print(plant)
-    data = json.load(open("cure.json", "r", encoding="utf-8"))
-    return render_template("cure.html",plant=plant, disease=disease,data = data)
-
-@app.route('/sup')
-def sup():
-    return render_template("supportform.html")
-
-@app.route('/support',methods=["POST"])
-def insert():
-    try:
-        if 1:#request.method == "POST":
-            info = request.form
-            rec = {}
-            #u_id = info['user_id']
-            #info.pop('user_id',None)
-            #del info['user_id']
-            print("************")
-            #info = json.dumps(info)
-            print(request.form)
-            rec['user_id'] = info['user_id']
-            rec['disease_title'] = info['disease_title']
-            rec['causes']=info['causes']
-            rec['cure']=info['cure']
-            rec['text']=info['text']
-            db.support.insert_one(rec)
-            print(rec)
-            return render_template("index.html")
-
-    except Exception as e:
-        print(e)
-        return render_template("index.html")
-
+    return render_template("upload.html")
 
 @app.route('/logout')
 def logout():
@@ -155,4 +235,4 @@ def logout():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=80)
+    app.run(debug=True,host="0.0.0.0",port=5000)
