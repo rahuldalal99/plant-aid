461a46d26e31decad3fdc06c69ad3522907d658a
root <tanaykarve@gmail.com>
2019-12-04 13:24:05 +0000

    added init file

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 apt-get install python3 python3-pip
-pip3 install requirements.txt
-gunicorn -b 0.0.0.0 -p 80 app.py --daemon
+pip3 install --upgrade pip
+pip install -r requirements.txt
+gunicorn -b 0.0.0.0:80 app:app --daemon
 
