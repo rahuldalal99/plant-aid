186a65dcb52c66ca21e93167b6546398c7fd4243
root <tanaykarve@gmail.com>
2020-02-27 16:00:11 +0000

    added run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 8501:8501 --mount type=bind,source=/root/saved_models,target=/models/predictor --name medivine-vision -e MODEL_NAME=predictor -t tensorflow/serving  
+docker run --rm -p 8501:8501 --mount type=bind,source=$(pwd)/saved_model,target=/models/predictor --name medivine-vision -e MODEL_NAME=predictor -t tensorflow/serving & 
